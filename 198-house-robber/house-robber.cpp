6aab5b85a2db69d7291e2d28aca0847f6ca19707
TanmayNull <gautamyash2006@gmail.com>
2026-09-04 12:47:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (71.28%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int chori(vector<int>&nums,int ind,vector<int>&dp){
-        if(ind==0)
-        return nums[ind];
-        if(ind<0)
-        return 0;
-        if(dp[ind]!=-1)
-        return dp[ind];
-        int pick = nums[ind]+chori(nums,ind-2,dp);
-        int np = 0+chori(nums,ind-1,dp);
-        return dp[ind]=max(pick,np);
-    } 
+int func(vector<int>& nums,int ind,vector<int>&dp)
+{
+    if(ind==0)
+    return nums[ind];
+    if(ind<0)
+    return 0;
+    if(dp[ind]!=-1)
+    return dp[ind];
+    int left = nums[ind]+func(nums,ind-2,dp);
+    int right = 0+func(nums,ind-1,dp);
+    return dp[ind]= max(left,right);
+}
     int rob(vector<int>& nums) {
-        int n= nums.size();
+        int n = nums.size();
         vector<int>dp(n+1,-1);
-        return chori(nums,nums.size()-1,dp);
+        return func(nums,n-1,dp);
     }
 };
