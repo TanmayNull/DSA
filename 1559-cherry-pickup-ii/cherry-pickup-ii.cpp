5937d345aee0b8fb789ee7b2d32959e0018632ce
TanmayNull <gautamyash2006@gmail.com>
2026-09-06 13:10:40 +0530

    Time: 48 ms (54.14%) | Memory: 21 MB (30.55%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   /* int path(vector<vector<int>>& grid,int i,int j1,int j2,int m,int n,vector<vector<vector<int>>>&dp){
+    int path(vector<vector<int>>& grid,int i,int j1,int j2,int m,int n,vector<vector<vector<int>>>&dp){
         if(j1<0||j2>n||j1>n||j2<0)
         {
             return -1e8;
@@ -26,41 +26,11 @@ public:
             }
         }
         return dp[i][j1][j2]=maxi;
-    }*/
+    }
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,0)));
-        for(int j1=0;j1<n;j1++){
-            for(int j2=0;j2<n;j2++){
-                if(j1==j2)
-                dp[m-1][j1][j2]=grid[m-1][j2];
-                else
-                dp[m-1][j1][j2]=grid[m-1][j1]+grid[m-1][j2];
-            }
-        }
-        
-        for(int i=m-2;i>=0;i--){
-            for(int j1=0;j1<n;j1++){
-                for(int j2=0;j2<n;j2++){  
-                    int maxi=-1e8;            
-                    for(int k1=-1;k1<=1;k1++){
-                        for(int k2=-1;k2<=1;k2++){
-                        int value=0;
-                        if(j1==j2)
-                        value=grid[i][j1];
-                        else
-                        value=grid[i][j1]+grid[i][j2];
-                        if(j1+k1<n&&j2+k2<n&&j1+k1>=0&&j2+k2>=0)
-                        value+=dp[i+1][j1+k1][j2+k2];
-                        else value+=-1e8;
-                        maxi= max(maxi,value);
-                        }
-                    }
-                    dp[i][j1][j2]=maxi;
-                }
-            }
-        }
-        return dp[0][0][n-1];
+        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
+        return path(grid,0,0,n-1,m-1,n-1,dp);
     }
 };
